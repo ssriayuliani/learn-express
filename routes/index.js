6c100cbfc5684d5a36ec120670f46150fb677c4d
Lia <sriayuliani1514@gmail.com>
2021-09-23 02:50:40 +0800

    ensureAuth dashboard go to the login form when window refresh

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
+// to protect 
+const {ensureAuthenticated} = require('../config/auth') 
 
 // welcome page
 router.get('/', (req, res)=>{
     res.render('welcome')
 })
 // dashboard
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    res.render('dashboard', {
+        user: req.user
+    })
 })
-
 
 module.exports = router
