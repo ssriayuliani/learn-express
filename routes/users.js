3c0314514dbf60e4025bfb5f67b85141f031656d
Lia <sriayuliani1514@gmail.com>
2021-09-22 22:57:26 +0800

    post user /register

--- routes/users.js
+++ routes/users.js
@@ -12,9 +12,38 @@ router.get("/register", (req, res) => {
 });
 
 // register handle
-router.post('/register', (req, res)=>{
-  console.log(req.body)
-  res.send('hello')
-})
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = []
+
+  // check required fields
+  if(!name || !email || !password || !password2){
+    errors.push({msg: 'Please fill in all fields'})
+  }
+
+  // check passwords match
+  if(password !== password2){
+    errors.push({msg: 'Passwords do not match'})
+  }
+
+  // check pass length
+  if(password.length < 6){
+    errors.push({msg: 'Password should be at least 6 characters'})
+  }
+
+  if(errors.length> 0){
+    // error -> rerender register -> pass in value: errors and the data
+    // -> dont want to the display empty data that has been checked at register.ejs
+    res.render('register', {
+      errors, 
+      name, 
+      email, 
+      password, 
+      password2
+    })
+  }else{
+    res.send('pass')
+  }
+});
 
 module.exports = router;
